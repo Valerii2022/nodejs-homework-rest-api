ceba69f5fe1559cc9d71d3bbcbeee41715673022
Valerii Pometun <vmpometun@gmail.com>
2023-07-18 14:04:27 +0300

    corrections

--- models/contacts.js
+++ models/contacts.js
@@ -1,62 +1,3 @@
-// import { readFile, writeFile } from "fs/promises";
-// import { nanoid } from "nanoid";
-// import path from "path";
-
-// const contactsPath = path.resolve("models", "contacts.json");
-
-// // -----------------------------------------------------------------------
-// const updateContactsExample = (contacts) =>
-//   writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-// // -----------------------------------------------------------------------------
-
-// export const listContacts = async () => {
-//   console.log("listContacts running");
-//   const data = await readFile(contactsPath, "utf-8");
-//   return JSON.parse(data);
-// };
-
-// export const getContactById = async (contactId) => {
-//   const contacts = await listContacts();
-//   const result = contacts.find((item) => item.id === contactId);
-//   return result || null;
-// };
-
-// export const removeContact = async (contactId) => {
-//   const contacts = await listContacts();
-//   const index = contacts.findIndex((item) => item.id === contactId);
-//   if (index === -1) {
-//     return null;
-//   }
-//   const [result] = contacts.splice(index, 1);
-//   // ================================================
-//   await updateContactsExample(contacts);
-//   // ==========================================
-//   return result;
-// };
-
-// export const addContact = async ({ name, email, phone }) => {
-//   const contacts = await listContacts();
-//   const newContact = {
-//     id: nanoid(),
-//     name,
-//     email,
-//     phone,
-//   };
-//   contacts.push(newContact);
-//   await updateContactsExample(contacts);
-//   return newContact;
-// };
-
-// export const updateContact = async (contactId, body) => {};
-
-// // export default {
-// //   listContacts,
-// //   getContactById,
-// //   removeContact,
-// //   addContact,
-// //   updateContact,
-// // };
-
 import fs from "fs/promises";
 import path from "path";
 import { nanoid } from "nanoid";
@@ -77,12 +18,13 @@ export const getContactById = async (id) => {
   return result || null;
 };
 
-export const addContact = async ({ title, director }) => {
+export const addContact = async ({ name, email, phone }) => {
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
-    title,
-    director,
+    name,
+    email,
+    phone,
   };
   contacts.push(newContact);
   await updateContactsStorage(contacts);
