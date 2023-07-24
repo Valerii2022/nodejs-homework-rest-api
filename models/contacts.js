3b83a2a7105b53a288fc7ec963e8d8cf396c5d4d
Valerii Pometun <vmpometun@gmail.com>
2023-07-24 09:10:57 +0300

    corrections

--- models/contacts.js
+++ models/contacts.js
@@ -1,62 +1,19 @@
-import fs from "fs/promises";
-import path from "path";
-import { nanoid } from "nanoid";
+// const fs = require('fs/promises')
 
-const contactsPath = path.resolve("models", "contacts.json");
+const listContacts = async () => {}
 
-const updateContactsStorage = (contacts) =>
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+const getContactById = async (contactId) => {}
 
-export const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
-};
+const removeContact = async (contactId) => {}
 
-export const getContactById = async (id) => {
-  const contacts = await listContacts();
-  const result = contacts.find((item) => item.id === id);
-  return result || null;
-};
+const addContact = async (body) => {}
 
-export const addContact = async ({ name, email, phone }) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await updateContactsStorage(contacts);
-  return newContact;
-};
+const updateContact = async (contactId, body) => {}
 
-export const updateContact = async (id, { name, email, phone }) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === id);
-  if (index === -1) {
-    return null;
-  }
-  contacts[index] = { id, name, email, phone };
-  await updateContactsStorage(contacts);
-  return contacts[index];
-};
-
-export const removeContact = async (id) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === id);
-  if (index === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  await updateContactsStorage(contacts);
-  return result;
-};
-
-export default {
+module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-};
+}
