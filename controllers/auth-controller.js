daafacf047a8d8af3702482147c8d1b2f064bb59
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-08-15 18:58:15 +0300

    fix mistake

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -63,7 +63,7 @@ const verifyEmail = async (req, res) => {
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
-    verificationToken: "",
+    verificationToken: null,
   });
 
   res.json({
